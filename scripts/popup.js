38e682a48df1244f59d4820b3e6eb4b998e1b8cb
arbass <13ar.bass@gmail.com>
2017-09-10 21:09:13 +0300

    Ошибки в контакнтах

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,16 +2,16 @@ $(document).ready(function(){
     PopUpHide();
 });
 function PopUpShow(){
-    $(".ad-full").show();
+    $(".popup").show();
 }
 function PopUpHide(){
-    $(".ad-full").hide();
+    $(".popup").hide();
 }
 
 
 jQuery(function($){
 	$(document).mouseup(function (e){ // событие клика по веб-документу
-		var div = $(".ad-full"); // тут указываем ID элемента
+		var div = $(".popup"); // тут указываем ID элемента
 		if (!div.is(e.target) // если клик был не по нашему блоку
 		    && div.has(e.target).length === 0) { // и не по его дочерним элементам
 			div.hide(); // скрываем его
