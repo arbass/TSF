618a0fbe9d2b0c83a19764b1ae6aa9c26045da73
Artem Kopytok <13ar.bass@gmail.com>
2017-09-17 19:16:55 +0300

    Карточка товара

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,35 +1,31 @@
-var slideIndex = 1;
+var slideIndex=1;
 showSlides(slideIndex);
 
-function plusSlide(n) {
+function plusSlides(n){
 	showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
+function currentSlide(n){
 	showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
+function showSlides(n){
 	var i;
 	var slides = document.getElementsByClassName("mySlides");
-	var dots = document.getElementsByClassName("dot"); 
+	var dots = document.getElementsByClassName("dot");
 
-	if (n > slides.length) {
-		slideIndex = 1
+	if(n > slides.length){
+		slideIndex = 1;
 	}
-
-	if (n < 1) {
-		slideIndex = slides.length
+	if(n < 1){
+		slideIndex = slides.length;
 	}
-
-	for (i=0; i<slides.length; i++) {
-		slides[i].style.display= "none";
+	for(i=0; i<slides.length; i++){
+		slides[i].style.display = "none";
 	}
-
-	for (var i = 0; i < dots.length; i++) {
-		dots[i].className= dots[i].className.replace("active","");
+	for(i=0; i < dots.length; i++){
+		dots[i].className = dots[i].className.replace("active","");
 	}
-
-	slides[slideIndex-1].style.display = "block";
-	dots[slideIndex-1].className+= "active";
+	 slides[slideIndex-1].style.display = "block";
+	 dots[slideIndex-1].className+= " active";
 }
